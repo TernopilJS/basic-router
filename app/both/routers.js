cf311474190068ac50e28652c9322b64108af248
maxim <uzumaxy@gmail.com>
2015-03-19 16:29:43 +0200

    Added server-side routers.

--- app/both/routers.js
+++ app/both/routers.js
@@ -32,3 +32,11 @@ Router.route('news', {
 });
 
 Router.route('aboutUs');
+
+Router.route('/restful', {where: 'server'})
+  .get(function () {
+    this.response.end('get request\n');
+  })
+  .post(function () {
+    this.response.end('post request\n');
+  });
