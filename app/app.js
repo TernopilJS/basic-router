e537a4262d0f4d494f4dfbd8cbb35e5a9a9fc4d0
maxim <uzumaxy@gmail.com>
2015-03-19 15:09:02 +0200

    Data context.

--- app/app.js
+++ app/app.js
@@ -1,6 +1,21 @@
 Router.route('home', {
   path: '/',
-  template: 'Home'
+  template: 'Home',
+  data: {
+    news: [{
+      dateTime: 'December 19, 2014',
+      text: 'December Devshop SF: Advances in MongoDB scalability, Meteor + Polymer, reactive MySQL, and more'
+    }, {
+      dateTime: 'December 10, 2014',
+      text: 'Meteor Day Recap: 4,259 developers at 134 meetups on one day'
+    }, {
+      dateTime: 'December 9, 2014',
+      text: 'Meteor 1.0.1: security fix for allow/deny rules'
+    }, {
+      dateTime: 'October 28, 2014',
+      text: 'Meteor 1.0'
+    },]
+  }
 });
 
 Router.route('aboutUs');
